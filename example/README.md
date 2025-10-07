6e3a14d36ff8809ca7a082e60b3e0ba95558a8ca
Lucas Branco <lucas.branco@aboutyou.com>
2025-10-07 14:29:44 +0200

    Updating flutter example project

--- example/README.md
+++ example/README.md
@@ -1,4 +1,4 @@
-# example
+# fabula_example
 
 A new Flutter project.
 
---
+++ example/android/app/src/main/kotlin/com/itzbala/fabula_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.itzbala.fabula_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
