2783dcc90711bdf4f19dcca110ce89fca620cb2f
Lucas Branco <lucas.branco@aboutyou.com>
2025-12-17 12:08:14 +0100

    ExhibitPage -> ExhibitRaw

---
+++ lib/src/exhibit_raw.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:storyto/src/knob_manager.dart';
+import 'package:storyto/storyto.dart';
+
+enum ExhibitPageKnobPosition {
+  inPage,
+  inDrawer,
+}
+
+class ExhibitRaw extends StatefulWidget {
+  const ExhibitRaw({
+    super.key,
+    required this.builder,
+    this.knobPosition = ExhibitPageKnobPosition.inPage,
+  });
+
+  final KnobBuilder builder;
+  final ExhibitPageKnobPosition knobPosition;
+
+  @override
+  State<ExhibitRaw> createState() => _ExhibitRawState();
+}
+
+class _ExhibitRawState extends State<ExhibitRaw> {
+  late final knobManager = KnobManager();
+
+  @override
+  void dispose() {
+    knobManager.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListenableBuilder(
+          listenable: knobManager.rebuildExhibit,
+          builder: (_, __) => widget.builder(knobManager),
+        ),
+        ListenableBuilder(
+          listenable: knobManager.rebuildKnobs,
+          builder: (_, __) => Column(
+            children: knobManager.knobs.values
+                .map(
+                  (e) => ListenableBuilder(
+                    listenable: e,
+                    builder: (_, __) => Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 8,
+                      ),
+                      child: e.knob(),
+                    ),
+                  ),
+                )
+                .toList(),
+          ),
+        )
+      ],
+    );
+  }
+}
