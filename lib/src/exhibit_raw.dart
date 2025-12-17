00de6ba9f8548bdd48525c1e8a23acc49e44c11e
Lucas Branco <lucas.branco@aboutyou.com>
2025-12-17 16:44:26 +0100

    exhibit_raw to support full screen pages and showing knobs

--- lib/src/exhibit_raw.dart
+++ lib/src/exhibit_raw.dart
@@ -21,6 +21,12 @@ class ExhibitRaw extends StatefulWidget {
 
 class _ExhibitRawState extends State<ExhibitRaw> {
   late final knobManager = KnobManager();
+  bool _showKnobs = true;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   void dispose() {
@@ -30,31 +36,72 @@ class _ExhibitRawState extends State<ExhibitRaw> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ListenableBuilder(
-          listenable: knobManager.rebuildExhibit,
-          builder: (_, __) => widget.builder(knobManager),
-        ),
-        ListenableBuilder(
-          listenable: knobManager.rebuildKnobs,
-          builder: (_, __) => Column(
-            children: knobManager.knobs.values
-                .map(
-                  (e) => ListenableBuilder(
-                    listenable: e,
-                    builder: (_, __) => Padding(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 8,
-                      ),
-                      child: e.knob(),
-                    ),
-                  ),
-                )
-                .toList(),
+    return Material(
+      child: Stack(
+        children: [
+          Positioned.fill(
+            bottom: 0,
+            child: GestureDetector(
+              onTap: () => setState(() => _showKnobs = false),
+              child: ListenableBuilder(
+                listenable: knobManager.rebuildExhibit,
+                builder: (_, __) => widget.builder(knobManager),
+              ),
+            ),
           ),
-        )
-      ],
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 300),
+            left: _showKnobs ? MediaQuery.of(context).size.width * 0.8 : 0,
+            top: MediaQuery.of(context).size.height / 2,
+            child: GestureDetector(
+              onTap: () => setState(() => _showKnobs = !_showKnobs),
+              child: Container(
+                width: 18,
+                height: 28,
+                decoration: const BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.horizontal(
+                    right: Radius.circular(14),
+                  ),
+                ),
+                child: const Icon(
+                  Icons.chevron_right,
+                  size: 16,
+                ),
+              ),
+            ),
+          ),
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 300),
+            bottom: 0,
+            top: 0,
+            left: _showKnobs ? 0 : -MediaQuery.of(context).size.width * 0.8,
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: ListenableBuilder(
+              listenable: knobManager.rebuildKnobs,
+              builder: (_, __) => Container(
+                color: Colors.white,
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: ListView(
+                  children: knobManager.knobs.values
+                      .map(
+                        (e) => ListenableBuilder(
+                          listenable: e,
+                          builder: (_, __) => Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 8,
+                            ),
+                            child: e.knob(),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
