86c144efa9122ca8b72dd4a05f2ece08e5bd9e85
Lucas Branco <lucas.branco@aboutyou.com>
2025-12-22 16:30:04 +0100

    KnobManager extension

---
+++ lib/src/helpers/knob_manager_extensions.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:storyto/src/knob_manager.dart';
+
+extension KnobManagerExtensions on KnobManager {
+  List<Widget> viewKnobs(WidgetBuilder builder) {
+    return knobs.values
+        .map(
+          (e) => ListenableBuilder(
+            listenable: e,
+            builder: (context, __) => e.knob(),
+          ),
+        )
+        .toList();
+  }
+}
